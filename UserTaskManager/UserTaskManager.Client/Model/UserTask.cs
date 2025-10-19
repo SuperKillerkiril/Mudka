6d48c9b94360350a62b1eba53ac0ffaac8743733
Bodya <udalovbg7@gmail.com>
2025-10-19 19:05:51 +0400

    123

--- UserTaskManager/UserTaskManager.Client/Model/UserTask.cs
+++ UserTaskManager/UserTaskManager.Client/Model/UserTask.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using MudBlazor;
+using Color = UserTaskManager.Enum.Color;
 
 namespace UserTaskManager.Model;
 
@@ -8,7 +7,7 @@ public class UserTask
 {
     [Key] public string Id { get; set; } = Guid.NewGuid().ToString();
     public string? Name { get; set; }
-    public int? Priority { get; set; }
+    public int Priority { get; set; }
     public string? Description { get; set; }
-    public Color? TaskColor { get; set; }
+    public Color TaskColor { get; set; }
 }
--- UserTaskManager/UserTaskManager.Client/Services/UserTaskService.cs
+++ UserTaskManager/UserTaskManager.Client/Services/UserTaskService.cs
@@ -6,9 +6,19 @@ public class MyUserTaskService
 {
     public List<UserTask> UserTasks { get; set; } = new List<UserTask>();
 
-    public List<UserTask> GetUserTasks(int quantity)
+    public List<UserTask> GetAllUserTasks(int quantity)
     {
         return UserTasks.Take(quantity).ToList();
+    }
+
+    public UserTask GetUserTaskById(string id)
+    {
+        var task = UserTasks.FirstOrDefault(t => t.Id == id);
+        if (task == null)
+        {
+            return null!;
+        }
+        return task;
     }
 
     public void AddListUserTasks(List<UserTask> userTasks) //Создание нескольких
@@ -40,9 +50,9 @@ public class MyUserTaskService
         }
     }
 
-    public void RedactUserTaskById(string id, UserTask userTask) //Редактирование по Id
+    public void RedactUserTaskById( UserTask userTask) //Редактирование по Id
     {
-        var index = UserTasks.FindIndex(t => t.Id == id);
+        var index = UserTasks.FindIndex(t => t.Id == userTask.Id);
         UserTasks[index] = userTask;
     }
 }
